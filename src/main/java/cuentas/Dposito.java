9104705514be28b9309e226470aa2915c18ad0b4
LAURA <LAURA@DESKTOP-1CLGRS4>
2024-02-09 21:23:11 +0100

    COMMIT 2

---
+++ src/main/java/cuentas/Dposito.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cuentas;
+
+import cuentas.CCuenta;
+import static cuentas.CCuenta.operativa_cuenta;
+
+/**
+ *
+ * @author LAURA
+ */
+public class Dposito {
+
+    public static void main(String[] args) {
+         operativa_cuenta(3);
+    }
+
+ 
+    }
+
