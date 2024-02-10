25f6b17f521c727a02fc06cce32e9468ddf52d12
LAURA <LAURA@DESKTOP-1CLGRS4>
2024-02-10 23:30:21 +0100

    COMENTARIO SOBRE EJECUCCION

--- src/main/java/cuentas/CCuenta.java
+++ src/main/java/cuentas/CCuenta.java
@@ -58,16 +58,25 @@ public class CCuenta {
         this.tipoInterés = tipoInterés;
     }
 
-
+ /**
+  * Atributos de la clase CCuenta
+  */
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
-
+//Constructor 
     public CCuenta()
     {
     }
-
+/**
+     * Constructor de la clase CCuenta que inicializa los atributos con los valores proporcionados.
+     *
+     * @param nom   El nombre del titular de la cuenta.
+     * @param cue   El número de cuenta.
+     * @param sal   El saldo inicial de la cuenta.
+     * @param tipo  El tipo de interés de la cuenta.
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -75,18 +84,34 @@ public class CCuenta {
         saldo=sal;
     }
 
+
+    /**
+     * Método para obtener el estado actual de la cuenta.
+     *
+     * @return El saldo actual de la cuenta.
+     */
     public double estado()
     {
         return getSaldo();
     }
-
+    /**
+     * Método para ingresar una cantidad en la cuenta.
+     *
+     * @param cantidad La cantidad a ingresar en la cuenta.
+     * @throws Exception Si se intenta ingresar una cantidad negativa.
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
-
+    /**
+     * Método para retirar una cantidad de la cuenta.
+     *
+     * @param cantidad La cantidad a retirar de la cuenta.
+     * @throws Exception Si se intenta retirar una cantidad negativa o si no hay suficiente saldo.
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -96,16 +121,19 @@ public class CCuenta {
         setSaldo(getSaldo() - cantidad);
     }
     
-    
-    
+/**
+     * Método para realizar operaciones (retiradas e ingresos) en la cuenta de ejemplo.
+     *
+     * @param Cantidad La cantidad a utilizar en la operativa de la cuenta.
+     */     
      public void  operativa_cuenta(float Cantidad) {
         CCuenta cuenta1;
         double saldoActual;
-        
+//Creación de una cuenta        
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
-
+//Intento de retirar una cantidad de la cuenta 
         try {
             cuenta1.retirar(2300);
         } catch (Exception e) {
